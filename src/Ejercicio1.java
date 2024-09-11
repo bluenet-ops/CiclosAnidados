cdb4f951a7ba9bdb85449c1a2bd995b030cad61a
Roberto Díaz <rdiazcastro@me.com>
2024-09-11 15:41:05 -0600

    Se suben ejercicios 1,2 y 3 de Java

---
+++ src/Ejercicio1.java
@@ -0,0 +1,27 @@
+import java.util.Iterator;
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+		int num2 = 1;
+		int num4 = 1;
+		int num6 = 1;
+		
+		for (int num1 = 1; num2 < 4; num2++) {
+			System.out.print(num1);
+			System.out.println(num2);
+		} // for 11,12,12
+		
+		for (int num3 = 2; num4 < 4; num4++) {
+			System.out.print(num3);
+			System.out.println(num4);
+		} // for 21,22,23
+		
+		for (int num5 = 3; num6 < 4; num6++) {
+			System.out.print(num5);
+			System.out.println(num6);
+		} // for 31,32,33
+
+	} // main
+
+} // class Ejercicio1
---
+++ src/Ejercicio2.java
@@ -0,0 +1,34 @@
+
+public class Ejercicio2 {
+
+	public static void main(String[] args) {
+		String asterisc = "* ";
+		
+		for (int i = 0; i < 6; i++) {
+		    System.out.print(asterisc);
+		}
+		System.out.println();
+		for (int i = 0; i < 5; i++) {
+		    System.out.print(asterisc);
+		}
+		System.out.println();
+		for (int i = 0; i < 4; i++) {
+		    System.out.print(asterisc);
+		}
+		System.out.println();
+		for (int i = 0; i < 3; i++) {
+		    System.out.print(asterisc);
+		}
+		System.out.println();
+		for (int i = 0; i < 2; i++) {
+		    System.out.print(asterisc);
+		}
+		System.out.println();
+		for (int i = 0; i < 1; i++) {
+		    System.out.print(asterisc);
+		}
+		System.out.println();
+		
+	} // main
+
+} // class Ejercicio2
